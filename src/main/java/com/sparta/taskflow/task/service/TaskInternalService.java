37e92930511d49809095ec81fe0b974269ea4da0
eunseo04 <jes040616@naver.com>
2025-09-03 12:45:10 +0900

    feat: service interface 구현

--- src/main/java/com/sparta/taskflow/task/service/TaskInternalService.java
+++ src/main/java/com/sparta/taskflow/task/service/TaskInternalService.java
@@ -1,23 +1,10 @@
 package com.sparta.taskflow.task.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface TaskInternalService {
-    //Task 생성
-    Task create (String title, String description, LocalDateTime dueDate, String priority, Long assigneeId);
-
-    //Task 목록 조회
-    Page<Task> getTasks(String status, int page, int size, String search, Long assigneeId);
-
-    //Task 상세 조회
-    Task getById(Long id);
-
-    //Task 수정
-    Task update (String title, String description, LocalDateTime dueDate, String priority, String status, Long assigneeId);
-
-    //Task 상태 수정
-    Task updateStatus(String status);
-
-    //Task 삭제
-    void delete (Long id);
+    Task getByIdOrThrow(Long id);
+    List<Task> getAllById(Long id);
+    List<Task> getAllByIds(List<Long> ids);
 }
