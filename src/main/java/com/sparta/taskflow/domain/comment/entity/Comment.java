86080046815b69eea372901cf2b55965293103e7
nyong0313 <ghdyd6625@naver.com>
2025-09-03 14:04:43 +0900

    fix(comment): entity 관련 어노테이션 수정

--- src/main/java/com/sparta/taskflow/domain/comment/entity/Comment.java
+++ src/main/java/com/sparta/taskflow/domain/comment/entity/Comment.java
@@ -2,10 +2,16 @@ package com.sparta.taskflow.domain.comment.entity;
 
 import com.sparta.taskflow.domain.task.entity.Task;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
