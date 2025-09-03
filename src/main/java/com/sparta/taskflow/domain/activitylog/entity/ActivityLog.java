f774fd45b60dc0b39af1766cee4bcfb580235518
nyong0313 <ghdyd6625@naver.com>
2025-09-03 12:02:23 +0900

    feat(activity-log): ActivityLog entity skeleton 추가

---
+++ src/main/java/com/sparta/taskflow/domain/activitylog/entity/ActivityLog.java
@@ -0,0 +1,36 @@
+package com.sparta.taskflow.domain.activitylog.entity;
+
+import com.sparta.taskflow.common.entity.CreatedAtEntity;
+import com.sparta.taskflow.domain.activitylog.enums.ActivityType;
+import com.sparta.taskflow.domain.task.entity.Task;
+import com.sparta.taskflow.domain.task.entity.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ActivityLog extends CreatedAtEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ActivityType type;
+
+    @Size(max = 85)
+    @Column(nullable = false, length = 255)
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id", nullable = false)
+    private Task task;
+}
