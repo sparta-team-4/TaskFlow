e025d86a179ac58417365762b545fa296958cd3c
유현 <pyh5421@naver.com>
2025-09-03 14:35:22 +0900

    refactor(BaseEntity).java

--- src/main/java/com/sparta/taskflow/common/BaseEntity.java
+++ src/main/java/com/sparta/taskflow/common/BaseEntity.java
@@ -16,11 +16,9 @@ public class BaseEntity {
 
     @CreatedDate
     @Column(updatable = false)
-    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime updatedAt;
 
 }
