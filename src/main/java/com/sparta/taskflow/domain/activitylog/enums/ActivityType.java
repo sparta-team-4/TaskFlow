9f9c76e4c1b05ba479d91a428aa945b41894cdbd
nyong0313 <ghdyd6625@naver.com>
2025-09-03 12:02:23 +0900

    feat(activity-log): ActivityLog entity skeleton 추가

---
+++ src/main/java/com/sparta/taskflow/domain/activitylog/enums/ActivityType.java
@@ -0,0 +1,11 @@
+package com.sparta.taskflow.domain.activitylog.enums;
+
+public enum ActivityType {
+    TASK_CREATED,
+    TASK_UPDATED,
+    TASK_DELETED,
+    TASK_STATUS_CHANGED,
+    COMMENT_CREATED,
+    COMMENT_UPDATED,
+    COMMENT_DELETED;
+}
