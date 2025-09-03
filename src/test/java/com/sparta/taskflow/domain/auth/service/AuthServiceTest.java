62f5284695bd7e73b1a9310abe830c49bcc91aff
seyoung5744 <seyoung574458@gmail.com>
2025-09-03 20:12:41 +0900

    test(auth) : 회원 가입 성공 및 사용자명 중복 에러 테스트

---
+++ src/test/java/com/sparta/taskflow/domain/auth/service/AuthServiceTest.java
@@ -0,0 +1,90 @@
+package com.sparta.taskflow.domain.auth.service;
+
+import com.sparta.taskflow.common.enums.Role;
+import com.sparta.taskflow.domain.auth.dto.request.UserRegisterRequest;
+import com.sparta.taskflow.domain.auth.dto.response.UserRegisterResponse;
+import com.sparta.taskflow.domain.user.entity.User;
+import com.sparta.taskflow.domain.user.exception.DuplicateUsernameException;
+import com.sparta.taskflow.domain.user.repository.UserRepository;
+import com.sparta.taskflow.utils.TestUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class AuthServiceTest {
+
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    AuthService authService;
+
+    @Test
+    @DisplayName("회원가입에 성공한다.")
+    void signUp_success() {
+        // given
+        UserRegisterRequest request = new UserRegisterRequest(
+                "test",
+                "won se",
+                "test@test.com",
+                "password1234!"
+        );
+        LocalDateTime createdAt = LocalDateTime.of(2025, 9, 3, 19, 48);
+        User user = TestUtils.createEntity(User.class, Map.of(
+                "id", 1L,
+                "username", "test",
+                "email", "test@test.com",
+                "name", "won se",
+                "role", Role.USER,
+                "createdAt", createdAt
+        ));
+
+        given(userRepository.existsByUsername(anyString())).willReturn(false);
+        given(userRepository.save(any(User.class))).willReturn(user);
+
+        // when
+        UserRegisterResponse response = authService.signUp(request);
+
+        // then
+        assertThat(response)
+                .extracting("id", "username", "email", "name", "role", "createdAt")
+                .contains(1L, "test", "test@test.com", "won se", Role.USER, createdAt);
+
+        verify(userRepository, times(1)).existsByUsername(anyString());
+        verify(userRepository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    @DisplayName("이미 존재하는 사용자명이 있으면 회원가입을 할 수 없다.")
+    void signUp_fail_whenDuplicateUsername() {
+        // given
+        UserRegisterRequest request = new UserRegisterRequest(
+                "test",
+                "won se",
+                "test@test.com",
+                "password1234!"
+        );
+
+        given(userRepository.existsByUsername(anyString())).willReturn(true);
+
+        // when & then
+        assertThatThrownBy(() -> authService.signUp(request))
+                .isInstanceOf(DuplicateUsernameException.class)
+                .hasMessage("이미 존재하는 사용자명입니다");
+    }
+}
