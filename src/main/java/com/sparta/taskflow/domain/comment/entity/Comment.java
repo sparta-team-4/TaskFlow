0b1ab6f17e453bff0e03987fea98e90232433bbd
nyong0313 <ghdyd6625@naver.com>
2025-09-03 12:49:20 +0900

    feat(comment): Comment entity 추가

---
+++ src/main/java/com/sparta/taskflow/domain/comment/entity/Comment.java
@@ -0,0 +1,31 @@
+package com.sparta.taskflow.domain.comment.entity;
+
+import com.sparta.taskflow.common.entity.BaseEntity;
+import com.sparta.taskflow.domain.task.entity.Task;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Comment extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id", nullable = false)
+    private Task task;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Comment parentComment;
+
+    @OneToMany(mappedBy = "parentComment", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> childComments = new ArrayList<>();
+}
