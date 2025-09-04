5cb72a7ba048a0f653424705972cf1430df70a25
eunseo04 <jes040616@naver.com>
2025-09-05 00:17:41 +0900

    refactor: "쿼리 수정"

--- src/main/java/com/sparta/taskflow/domain/task/controller/TaskController.java
+++ src/main/java/com/sparta/taskflow/domain/task/controller/TaskController.java
@@ -7,13 +7,10 @@ import com.sparta.taskflow.domain.task.enums.TaskStatus;
 import com.sparta.taskflow.domain.task.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
