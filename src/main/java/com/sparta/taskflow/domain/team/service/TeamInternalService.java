26f42878081ba5159a195faaab5fda8cd9d3150f
Park-yh <pyh5421@naver.com>
2025-09-04 10:46:03 +0900

    fix(service): 패키지 위치 변경 및 구조 수정

--- src/main/java/com/sparta/taskflow/domain/team/service/TeamInternalService.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.taskflow.domain.team.service;
-
-import com.sparta.taskflow.domain.team.dto.TeamRequestDto;
-import com.sparta.taskflow.domain.team.dto.TeamResponseDto;
-
-public interface TeamInternalService {
-    TeamResponseDto.Create createTeam(TeamRequestDto.Create requestDto);
-}
