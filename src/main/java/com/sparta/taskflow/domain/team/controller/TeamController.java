9ea85d11adfb66d9ee006c90ca69469fcf06dff4
Park-yh <pyh5421@naver.com>
2025-09-04 10:40:54 +0900

    refactor(TeamController)

--- src/main/java/com/sparta/taskflow/domain/team/controller/TeamController.java
+++ src/main/java/com/sparta/taskflow/domain/team/controller/TeamController.java
@@ -3,7 +3,7 @@ package com.sparta.taskflow.domain.team.controller;
 import com.sparta.taskflow.common.response.ApiResponse;
 import com.sparta.taskflow.domain.team.dto.TeamRequestDto;
 import com.sparta.taskflow.domain.team.dto.TeamResponseDto;
-import com.sparta.taskflow.domain.team.service.TeamService;
+import com.sparta.taskflow.domain.team.service.external.TeamCommandService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,16 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TeamController {
 
-    private final TeamService teamService;
+    private final TeamCommandService teamCommandService;
 
     @PostMapping
     public ResponseEntity<ApiResponse<TeamResponseDto.Create>> createTeam(
             @RequestBody TeamRequestDto.Create requestDto) {
 
-        // 서비스 로직을 호출하여 팀 생성
-        TeamResponseDto.Create responseDto = teamService.createTeam(requestDto);
+        TeamResponseDto.Create responseDto = teamCommandService.createTeam(requestDto);
 
-        // ApiResponse의 정적 메서드를 사용하여 성공 응답 반환
         return ApiResponse.created(responseDto, "팀이 성공적으로 생성되었습니다.");
     }
 }
