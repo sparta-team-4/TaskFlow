68d7fc08291714adadbedd5f8db51c27d2e755f8
Park-yh <pyh5421@naver.com>
2025-09-04 21:34:38 +0900

    feat(team): 팀 조회, 수정, 삭제 기능 구현

--- src/main/java/com/sparta/taskflow/domain/team/repository/TeamRepository.java
+++ src/main/java/com/sparta/taskflow/domain/team/repository/TeamRepository.java
@@ -4,7 +4,10 @@ import com.sparta.taskflow.domain.team.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
     boolean existsByName(String name);
+    Optional<Team> findByName(String name);
 }
--- src/main/java/com/sparta/taskflow/domain/team/service/external/TeamQueryService.java
+++ src/main/java/com/sparta/taskflow/domain/team/service/external/TeamQueryService.java
@@ -1,6 +1,12 @@
 package com.sparta.taskflow.domain.team.service.external;
 
+import com.sparta.taskflow.domain.team.dto.TeamResponseDto;
+import com.sparta.taskflow.domain.team.entity.Team;
+import com.sparta.taskflow.domain.team.repository.TeamRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,4 +14,29 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class TeamQueryService {
+
+    private final TeamRepository teamRepository;
+
+    /**
+     * .
+     * @param teamId 조회할 팀의 ID
+     * @return 조회된 팀 정보 DTO
+     * @throws EntityNotFoundException 해당 ID의 팀이 없을 경우
+     */
+    public TeamResponseDto.Get getTeam(Long teamId) {
+        Team team = teamRepository.findById(teamId)
+                .orElseThrow(() -> new EntityNotFoundException("팀을 찾을 수 없습니다"));
+
+        return TeamResponseDto.Get.from(team);
+    }
+
+    /**
+     * 모든 팀의 목록을 페이징하여 조회
+     * @param pageable 페이징 정보
+     * @return 페이징된 팀 정보 DTO
+     */
+    public Page<TeamResponseDto.Get> getAllTeams(Pageable pageable) {
+        Page<Team> teams = teamRepository.findAll(pageable);
+        return teams.map(TeamResponseDto.Get::from);
+    }
 }
