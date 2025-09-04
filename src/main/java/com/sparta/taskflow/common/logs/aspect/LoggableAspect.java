956f654b42f684d41680eb62f67f91b59b739796
nyong0313 <ghdyd6625@naver.com>
2025-09-05 00:13:31 +0900

    feat(common): AOP 기반 자동 로깅 기능 추가

---
+++ src/main/java/com/sparta/taskflow/common/logs/aspect/LoggableAspect.java
@@ -0,0 +1,48 @@
+package com.sparta.taskflow.common.logs.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggableAspect {
+
+    private final Logger log = LoggerFactory.getLogger(LoggableAspect.class);
+
+    @Pointcut("@annotation(com.sparta.taskflow.common.logs.annotation.Loggable)")
+    private void loggableMethods() {}
+
+    @Around("loggableMethods()")
+    public Object log(ProceedingJoinPoint pjp) throws Throwable {
+
+        long startTime = System.currentTimeMillis();
+
+        String methodName = pjp.getSignature().toShortString();
+        Object[] args = pjp.getArgs();
+
+        String traceId = MDC.get("traceId");
+
+        try {
+            log.info("[{}] -> {} args={}", traceId, methodName, args);
+
+            Object result = pjp.proceed();
+
+            log.info("[{}] <- {} retType={}", traceId, methodName,
+                    (result == null ? "void" : result.getClass().getSimpleName()));
+
+            return result;
+        } catch (Throwable t) {
+            log.error("[{}] !! {} ex={} msg={}", traceId, methodName,
+                    t.getClass().getSimpleName(), t.getMessage());
+            throw t;
+        } finally {
+            log.info("[{}] took={}ms", traceId, System.currentTimeMillis() - startTime);
+        }
+    }
+}
