34496c97ce6567b6b76b97af8b110502bc4862d7
seyoung5744 <seyoung574458@gmail.com>
2025-09-04 15:04:22 +0900

    teat(auth) : 로그인 API 테스트

--- src/test/java/com/sparta/taskflow/domain/auth/service/AuthServiceTest.java
+++ src/test/java/com/sparta/taskflow/domain/auth/service/AuthServiceTest.java
@@ -1,13 +1,18 @@
 package com.sparta.taskflow.domain.auth.service;
 
 import com.sparta.taskflow.common.enums.Role;
+import com.sparta.taskflow.common.utils.JwtUtils;
+import com.sparta.taskflow.domain.auth.dto.request.LoginRequest;
 import com.sparta.taskflow.domain.auth.dto.request.UserRegisterRequest;
+import com.sparta.taskflow.domain.auth.dto.response.LoginResponse;
 import com.sparta.taskflow.domain.auth.dto.response.UserRegisterResponse;
+import com.sparta.taskflow.domain.auth.exception.DuplicateUsernameException;
+import com.sparta.taskflow.domain.auth.exception.InvalidUsernameOrPasswordException;
 import com.sparta.taskflow.domain.user.entity.User;
-import com.sparta.taskflow.domain.user.exception.DuplicateUsernameException;
 import com.sparta.taskflow.domain.user.repository.UserRepository;
 import com.sparta.taskflow.utils.TestUtils;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -17,6 +22,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -33,6 +39,9 @@ class AuthServiceTest {
 
     @Mock
     PasswordEncoder passwordEncoder;
+
+    @Mock
+    JwtUtils jwtUtils;
 
     @InjectMocks
     AuthService authService;
@@ -92,4 +101,75 @@ class AuthServiceTest {
                 .isInstanceOf(DuplicateUsernameException.class)
                 .hasMessage("이미 존재하는 사용자명입니다");
     }
+
+    @Nested
+    @DisplayName("로그인")
+    class LoginTest {
+
+        @Test
+        @DisplayName("로그인에 성공하면 token 을 발행한다.")
+        void login_success() {
+            // given
+            String rawPassword = "password1234!";
+            String encodedPassword = "fjdsaklfjdsaonewoaf";
+            LoginRequest request = new LoginRequest("test", rawPassword);
+            User user = TestUtils.createEntity(User.class, Map.of(
+                    "id", 1L,
+                    "username", "test",
+                    "password", encodedPassword,
+                    "role", Role.USER
+            ));
+
+            String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1eW5CaWlyY25ad3NSLzFHOWxMYmw5Q1dWZ25BcHJCY0lqWDNzZG4yWHRjPSIsImV4cCI6MTc1Njk1NzcxMiwiaWF0IjoxNzU2OTU1OTEyfQ.Mf4CysmSxaQqTf0o8xpQXzyM-AK9o3L_3dj5V-Nrzqs";
+            given(userRepository.findByUsername(anyString())).willReturn(Optional.of(user));
+            given(passwordEncoder.matches(rawPassword, encodedPassword)).willReturn(true);
+            given(jwtUtils.createToken(anyLong(), any(Role.class))).willReturn(token);
+
+            // when
+            LoginResponse response = authService.login(request);
+
+            // then
+            assertThat(response.token()).isEqualTo(token);
+        }
+
+        @Test
+        @DisplayName("유저 ID가 일치하지 않으면 로그인이 불가능하다.")
+        void login_fail_whenInvalidUsername() {
+            // given
+            LoginRequest request = new LoginRequest("test", "password1234!");
+
+            // when
+            when(userRepository.findByUsername(request.username())).thenReturn(Optional.empty());
+
+            // then
+            assertThatThrownBy(() -> authService.login(request))
+                    .isInstanceOf(InvalidUsernameOrPasswordException.class)
+                    .hasMessage("잘못된 사용자명 또는 비밀번호입니다");
+        }
+
+        @Test
+        @DisplayName("비밀번호가 일치하지 않으면 로그인이 불가능하다.")
+        void login_fail_whenInvalidPassword() {
+            // given
+            String rawPassword = "password1234!";
+            String encodedPassword = "fjdsaklfjdsaonewoaf";
+            LoginRequest request = new LoginRequest("test", rawPassword);
+            User user = TestUtils.createEntity(User.class, Map.of(
+                    "id", 1L,
+                    "username", "test",
+                    "password", encodedPassword,
+                    "role", Role.USER
+            ));
+            given(userRepository.findByUsername(anyString())).willReturn(Optional.of(user));
+
+            // when
+            when(passwordEncoder.matches(rawPassword, encodedPassword)).thenReturn(false);
+
+            // then
+            assertThatThrownBy(() -> authService.login(request))
+                    .isInstanceOf(InvalidUsernameOrPasswordException.class)
+                    .hasMessage("잘못된 사용자명 또는 비밀번호입니다");
+        }
+    }
+
 }
