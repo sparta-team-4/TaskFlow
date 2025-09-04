d7dfdb85318576beba147970107a66275ebc5a19
seyoung5744 <seyoung574458@gmail.com>
2025-09-04 15:03:40 +0900

    feat(auth) : 로그인 API

--- src/main/java/com/sparta/taskflow/domain/auth/controller/AuthController.java
+++ src/main/java/com/sparta/taskflow/domain/auth/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.sparta.taskflow.domain.auth.controller;
 
 import com.sparta.taskflow.common.response.ApiResponse;
+import com.sparta.taskflow.domain.auth.dto.request.LoginRequest;
 import com.sparta.taskflow.domain.auth.dto.request.UserRegisterRequest;
+import com.sparta.taskflow.domain.auth.dto.response.LoginResponse;
 import com.sparta.taskflow.domain.auth.dto.response.UserRegisterResponse;
 import com.sparta.taskflow.domain.auth.service.AuthService;
 import jakarta.validation.Valid;
@@ -24,6 +26,14 @@ public class AuthController {
         return ApiResponse.created(
                 authService.userRegister(request),
                 "회원가입이 완료되었습니다."
+        );
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<LoginResponse>> login(@Valid @RequestBody LoginRequest request) {
+        return ApiResponse.success(
+                authService.login(request),
+                "로그인이 완료되었습니다."
         );
     }
 }
---
+++ src/main/java/com/sparta/taskflow/domain/auth/dto/request/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.sparta.taskflow.domain.auth.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record LoginRequest(
+        @NotBlank(message = "로그인 ID는 필수입니다.")
+        @Pattern(regexp = "^[a-zA-Z0-9]{4,20}$", message = "로그인 ID는 4~20자의 영문/숫자만 가능합니다.")
+        String username,
+
+        @NotBlank(message = "비밀 번호는 필수 입력값입니다.")
+        @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,}", message = "비밀번호는 8자 이상 영문, 숫자, 특수문자를 사용하세요.")
+        String password
+) {
+}
