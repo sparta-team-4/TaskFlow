78c13a371b4861d4f458b18b9430c350c3244669
seyoung5744 <seyoung574458@gmail.com>
2025-09-05 14:26:24 +0900

    feat(user) : userInternalService 구현

---
+++ src/main/java/com/sparta/taskflow/domain/user/service/UserInternalServiceImpl.java
@@ -0,0 +1,21 @@
+package com.sparta.taskflow.domain.user.service;
+
+import com.sparta.taskflow.domain.user.entity.User;
+import com.sparta.taskflow.domain.user.exception.UserErrorCode;
+import com.sparta.taskflow.domain.user.exception.UserNotFoundException;
+import com.sparta.taskflow.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserInternalServiceImpl implements UserInternalService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User getByIdOrThrow(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(UserErrorCode.USER_NOT_FOUND));
+    }
+}
